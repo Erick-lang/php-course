704de911cda5c2b13189abc23f3d781ecd9b5a92
Erick-lang <68664130+Erick-lang@users.noreply.github.com>
2020-08-01 14:27:46 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+</!DOCTYPE html>
+<//html>
+  <//head>
+    <//title>PHP Store<//title>
+  <//head>
+  <//body>
+    </?php
+    // Using two forward slashes we create comments in php.
+    // Within the php tag, we can insert values into our HTML.
+    echo "<//h1>Welcome!<//h1>";
+    ?>
+  <//body>
+<//html>
+
+</!DOCTYPE html>
+<//html>
+  <//head>
+    <//title>PHP Store<//title>
+  <//head>
+  <//body>
+    </?php
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<//h1>Welcome to ".$name."!<//h1>";
+      echo "<//h2>You have $".$credit." in your wallet.<//h2>";
+    ?>
+  <//body>
+<//html>
+
+</!DOCTYPE html>
+<//html>
+  <//head>
+    <//title>PHP Store<//title>
+  <//head>
+  <//body>
+    </?php
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<//h1>Welcome to ".$name."!<//h1>";
+      echo "<//h2>You have $".$credit." in your wallet.<//h2>";
+
+      $products['Computer']=750;
+      $products['Car']=15000;
+      $products['iPhone']=1000;
+      $products['Toaster']=75;
+
+      echo "<//p>A car costs $".$products['Car']."<//p>";
+    ?>
+  <//body>
+<//html>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,6 +63,16 @@
 
       echo "<h1>Welcome to ".$name."!</h1>";
       echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+      $products['Computer']=750;
+      $products['Car']=15000;
+      $products['iPhone']=1000;
+      $products['Toaster']=75;
+
+      foreach($products as $key => $value){
+        echo "<p>The ".$key." costs ".$value."</p>";
+      }
+
     ?>
   </body>
 </html>
