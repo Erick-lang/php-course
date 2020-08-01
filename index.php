3eb7addeb09b845cdaeceefa6b9ad91660aed0e8
Erick-lang <68664130+Erick-lang@users.noreply.github.com>
2020-08-01 14:41:18 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,81 @@
-</!DOCTYPE html>
-<//html>
-  <//head>
-    <//title>PHP Store<//title>
-  <//head>
-  <//body>
-    </?php
+<!--<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store></title>
+  </head>
+  <body>
+    <?php
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<//h1>Welcome!<//h1>";
+    echo "<h1>Welcome!</h1>";
     ?>
-  <//body>
-<//html>
+  </body>
+</html>
 
-</!DOCTYPE html>
-<//html>
-  <//head>
-    <//title>PHP Store<//title>
-  <//head>
-  <//body>
-    </?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
       $name = "PHP Store";
       $credit = 1000; 
 
-      echo "<//h1>Welcome to ".$name."!<//h1>";
-      echo "<//h2>You have $".$credit." in your wallet.<//h2>";
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
     ?>
-  <//body>
-<//html>
+  </body>
+</html>
 
-</!DOCTYPE html>
-<//html>
-  <//head>
-    <//title>PHP Store<//title>
-  <//head>
-  <//body>
-    </?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
       $name = "PHP Store";
       $credit = 1000; 
 
-      echo "<//h1>Welcome to ".$name."!<//h1>";
-      echo "<//h2>You have $".$credit." in your wallet.<//h2>";
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
 
       $products['Computer']=750;
       $products['Car']=15000;
       $products['iPhone']=1000;
       $products['Toaster']=75;
 
-      echo "<//p>A car costs $".$products['Car']."<//p>";
+      echo "<p>A car costs $".$products['Car']."</p>";
     ?>
-  <//body>
-<//html>
+  </body>
+</html>-->
+
+<!--<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+      $products['Computer']=750;
+      $products['Car']=15000;
+      $products['iPhone']=1000;
+      $products['Toaster']=75;
+
+      foreach($products as $key => $value){
+        echo "<p>The ".$key." costs ".$value."</p>";
+      }
+
+    ?>
+  </body>
+</html>-->
 
 <!DOCTYPE html>
 <html>
@@ -73,6 +99,13 @@
         echo "<p>The ".$key." costs ".$value."</p>";
       }
 
+      echo "<h2>Items you can afford</h2>";
+
+      foreach($products as $key => $value){
+        if($value <= $credit ){
+          echo "<p>".$key."</p>"; 
+        }
+      }
     ?>
   </body>
 </html>
